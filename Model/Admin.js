0b2c54aa258fa9a79a6ec9f2e835ebcd4ada57de
eishwezin <thandar789820@gmail.com>
2019-04-24 22:10:07 +0630

    change package.json

--- Model/Admin.js
+++ Model/Admin.js
@@ -1,5 +1,5 @@
-var mongoose=require ('mongoose');
-var bcrypt=require('bcrypt-nodejs');
+var mongoose = require ('mongoose');
+var bcrypt = require('bcrypt-nodejs');
 var Schema=mongoose.Schema;
 
 var AdminSchema= new Schema({
