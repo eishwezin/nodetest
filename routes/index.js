adeda339ced138fa6ff58eefe5e5db02ac609e0d
eishwezin <thandar789820@gmail.com>
2019-04-24 22:57:17 +0630

    change folder name

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var Admin=require('../model/Admin')
+var Admin=require('../model/Admin');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express framework' });
