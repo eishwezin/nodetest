c4df0e3bff3d3c882bbb15458b061d738f6d373b
eishwezin <thandar789820@gmail.com>
2019-04-24 19:48:22 +0630

    modify file

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var Admin=require('../model/Admin')
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express framework' });
 });
 
  router.get('/signup',function(req,res){
