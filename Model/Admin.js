2c7d68b569e64551193dda332c23858303ba6a95
eishwezin <thandar789820@gmail.com>
2019-04-24 20:23:32 +0630

    remove dateformat

--- Model/Admin.js
+++ Model/Admin.js
@@ -1,7 +1,7 @@
 var mongoose=require ('mongoose');
-var Schema=mongoose.Schema;
-var dateformat=require('dateformat');
 var bcrypt=require('bcrypt-nodejs');
+var Schema=mongoose.Schema;
+
 var AdminSchema= new Schema({
   name:{
     type:String,
